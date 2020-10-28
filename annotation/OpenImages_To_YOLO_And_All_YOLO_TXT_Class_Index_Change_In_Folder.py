e2c62015210ae2714f847ac70f40851e78346a9b
Asif Ahmed <public.quickgrid@gmail.com>
2020-10-28 17:21:40 +0600

    Update and rename All_YOLO_TXT_Class_Index_Change_In_Folder.py to OpenImages_To_YOLO_And_All_YOLO_TXT_Class_Index_Change_In_Folder.py

--- annotation/OpenImages_To_YOLO_And_All_YOLO_TXT_Class_Index_Change_In_Folder.py
+++ annotation/OpenImages_To_YOLO_And_All_YOLO_TXT_Class_Index_Change_In_Folder.py
@@ -1,6 +1,7 @@
 ## Change class index in txt file
 ## A crude implementation of yolo txt class index change
 
+
 import glob
 import numpy
 import os
@@ -10,11 +11,22 @@ label_list = glob.glob("../dataset/labels/*.txt")
 print(label_list)
 
 
+def convertBox(sz, box):
+    dw = 1./(sz[0])
+    dh = 1./(sz[1])
+    x = (box[0] + box[1])/2.0 - 1
+    y = (box[2] + box[3])/2.0 - 1
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
 
 for i in label_list:
   b = numpy.loadtxt(i, dtype=str, ndmin=2)
-
-
 
   img = cv2.imread("/content/dataset/images/" + os.path.basename(i).split(".")[0] + ".jpg")
   sz = img.shape 
@@ -31,6 +43,7 @@ for i in label_list:
 
 
       ## Replace a certain class index in yolo with another class index
+      ## Replace an openimages class below with condition to get yolo index
       if b[j][0] == "0":
         tmp = "1 " + str(x1) + " " + str(x2) + " " + str(x3) + " " + str(x4)
         
