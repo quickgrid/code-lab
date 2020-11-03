92b55b715fdbfd721ac1c86c7f4ab136407e33ff
Asif Ahmed <public.quickgrid@gmail.com>
2020-11-03 22:38:18 +0600

    Update selenium_requests_beautifulsoup_ecosia_auto_webpage_image_download.py

--- dataset/selenium_requests_beautifulsoup_ecosia_auto_webpage_image_download.py
+++ dataset/selenium_requests_beautifulsoup_ecosia_auto_webpage_image_download.py
@@ -1,12 +1,17 @@
 """
-Based on: https://github.com/ivangrov/YOLOv3-Series/blob/master/%5BPart%203%5D%20Get%20Images/get_images.py
+Based on: 
+    https://github.com/ivangrov/YOLOv3-Series/blob/master/%5BPart%203%5D%20Get%20Images/get_images.py
+
 Requirements: 
     lxml
     selenium
     beautifulsoup
     requests
     chrome driver from, https://chromedriver.chromium.org/downloads
-Description: Gets all `image urls` from ecosia.org for given search query, downloads those and saves to given directory.
+
+Description:
+    Gets all `image urls` from ecosia.org for given search query, downloads those and saves to given directory.
+    After running code, enter any character on terminal and press enter to start process.
 """
 
 
