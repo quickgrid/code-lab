43e8ad898e1f3bab26393853691e75f023d8f7de
Asif Ahmed <public.quickgrid@gmail.com>
2021-05-17 12:02:50 +0600

    Update pytorch_gan_template.py

--- pytorch/pytorch_gan_template.py
+++ pytorch/pytorch_gan_template.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.datasets as datasets
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 
@@ -23,6 +23,22 @@ class Generator(nn.Module):
     def forward(self):
         pass
 
+
+class CustomImageDataset(Dataset):
+    def __init__(self, root_dir, transform):
+        super(CustomImageDataset, self).__init__()
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = os.listdir(root_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.root_dir, self.images[idx])
+        image = Image.open(image_path)
+        return self.transform(image)    
+    
 
 class Trainer():
     def __init__(self):
