3453773bf05f0b6b01fd8508521bc1c68964e6f6
Asif Ahmed <public.quickgrid@gmail.com>
2022-01-29 21:15:26 +0600

    Attempt at adding data loading based on anchor, positive, negative for triplet loss.

--- code-lab/pytorch/pytorch_siamese_network.py
+++ code-lab/pytorch/pytorch_siamese_network.py
@@ -83,91 +83,64 @@ class SiameseDataset(Dataset):
         return self.class_names_len
 
     def __getitem__(self, idx: int) -> Dict[str, Union[Image.Image, int, bool]]:
-        """Alternates selection between similar class and different class images.
+        """In single step get anchor, positive and negative.
 
         Args:
             idx: A sub folder index.
 
         Returns:
-            Dictionary of two PIL.Image and two integer label.
+            A sample of data containing anchor, positive, negative image and their labels.
         """
-        current_class_name = self.class_idx_to_name_map[idx]
-        current_dir_file_count = self.class_name_file_count_list[current_class_name]
+        anchor_dir_class_name = self.class_idx_to_name_map[idx]
+        anchor_dir_file_count = self.class_name_file_count_list[anchor_dir_class_name]
 
-        # Alternate between images from same class.
-        if idx % 2 == 1:
-            random_image_idx1 = np.random.randint(low=0, high=current_dir_file_count)
-            random_image_idx2 = np.random.randint(low=0, high=current_dir_file_count)
+        anchor_image_idx = np.random.randint(low=0, high=anchor_dir_file_count)
+        positive_image_idx = np.random.randint(low=0, high=anchor_dir_file_count)
+        while anchor_image_idx == positive_image_idx:
+            positive_image_idx = np.random.randint(low=0, high=anchor_dir_file_count)
 
-            image1_path = os.path.join(
+        negative_image_folder_idx = np.random.randint(low=0, high=self.class_names_len)
+        negative_dir_class_name = self.class_idx_to_name_map[negative_image_folder_idx]
+        negative_dir_file_count = self.class_name_file_count_list[negative_dir_class_name]
+        negative_image_idx = np.random.randint(low=0, high=negative_dir_file_count)
+
+        anchor_image_path = os.path.join(
                 self.root_dir,
-                current_class_name,
-                self.class_name_to_images_list[current_class_name][random_image_idx1]
-            )
-            image2_path = os.path.join(
-                self.root_dir,
-                current_class_name,
-                self.class_name_to_images_list[current_class_name][random_image_idx2]
-            )
+                anchor_dir_class_name,
+                self.class_name_to_images_list[anchor_dir_class_name][anchor_image_idx]
+        )
+        positive_image_path = os.path.join(
+            self.root_dir,
+            anchor_dir_class_name,
+            self.class_name_to_images_list[anchor_dir_class_name][positive_image_idx]
+        )
+        negative_image_path = os.path.join(
+            self.root_dir,
+            negative_dir_class_name,
+            self.class_name_to_images_list[negative_dir_class_name][negative_image_idx]
+        )
 
-            label1 = idx
-            label2 = idx
+        anchor_label = idx
+        positive_label = idx
+        negative_label = negative_image_folder_idx
 
-            # assert label1 != label2, 'Something went wrong with file selection.'
+        anchor_image = Image.open(anchor_image_path)
+        postive_image = Image.open(positive_image_path)
+        negative_image = Image.open(negative_image_path)
 
-            image1 = Image.open(image1_path)
-            image2 = Image.open(image2_path)
-            if self.transform:
-                image1 = self.transform(image1)
-                image2 = self.transform(image2)
+        if self.transform:
+            anchor_image = self.transform(anchor_image)
+            postive_image = self.transform(postive_image)
+            negative_image = self.transform(negative_image)
 
-            sample = {
-                'image1': image1,
-                'image2': image2,
-                'label1': label1,
-                'label2': label2,
-                'same_type': True,
-            }
-
-        else:
-            random_image_idx2 = np.random.randint(low=0, high=self.class_names_len)
-            # Think of better logic to handle this.
-            while idx == random_image_idx2:
-                random_image_idx2 = np.random.randint(low=0, high=self.class_names_len)
-            different_class_name = self.class_idx_to_name_map[random_image_idx2]
-
-            random_image_idx1 = np.random.randint(low=0, high=current_dir_file_count)
-
-            image1_path = os.path.join(
-                self.root_dir,
-                current_class_name,
-                self.class_name_to_images_list[current_class_name][random_image_idx1]
-            )
-            image2_path = os.path.join(
-                self.root_dir,
-                different_class_name,
-                self.class_name_to_images_list[different_class_name][random_image_idx2]
-            )
-
-            label1 = idx
-            label2 = random_image_idx2
-
-            # assert label1 == label2, 'Something went wrong with file selection.'
-
-            image1 = Image.open(image1_path)
-            image2 = Image.open(image2_path)
-            if self.transform:
-                image1 = self.transform(image1)
-                image2 = self.transform(image2)
-
-            sample = {
-                'image1': image1,
-                'image2': image2,
-                'label1': label1,
-                'label2': label2,
-                'same_type': True,
-            }
-
+        sample = {
+            'anchor_image': anchor_image,
+            'postive_image': postive_image,
+            'negative_image': negative_image,
+            'anchor_label': anchor_label,
+            'positive_label': positive_label,
+            'negative_label': negative_label,
+        }
         return sample
 
 
@@ -188,7 +161,7 @@ class Trainer:
     def __init__(
             self,
             dataset_path: str,
-            batch_size: int = 6,
+            batch_size: int = 8,
             num_workers: int = 4,
             learning_rate: float = 0.0001,
             num_epochs: int = 10,
@@ -212,7 +185,6 @@ class Trainer:
         self.num_classes = len(os.listdir(dataset_path))
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = SiameseModel(num_classes=self.num_classes).to(self.device)
-        # self.loss_fn = torch.nn.CrossEntropyLoss().to(self.device)
         self.loss_fn = contrastive_loss_fn(margin=0.05)
         self.optimizer_fn = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
 
@@ -221,52 +193,64 @@ class Trainer:
             with tqdm(self.train_loader) as tqdm_epoch:
                 for batch_idx, data in enumerate(tqdm_epoch):
                     # print(batch_idx, data)
-                    image1 = data['image1']
-                    image2 = data['image2']
-                    label1 = data['label1']
-                    label2 = data['label2']
+                    anchor_image = data['anchor_image']
+                    postive_image = data['postive_image']
+                    negative_image = data['negative_image']
+                    anchor_label = data['anchor_label']
+                    positive_label = data['positive_label']
+                    negative_label = data['negative_label']
 
-                    image1 = image1.to(self.device)
-                    image2 = image2.to(self.device)
+                    anchor_image = anchor_image.to(self.device)
+                    postive_image = postive_image.to(self.device)
+                    negative_image = negative_image.to(self.device)
 
-                    print(image1.shape)
-                    print(image2.shape)
+                    print(anchor_image.shape)
+                    print(postive_image.shape)
+                    print(negative_image.shape)
 
                     # onehot_label1 = F.one_hot(label1)
                     # onehot_label2 = F.one_hot(label2)
 
-                    output = self.model.forward(image1, image2)
-                    print(output)
+                    positive_output = self.model.forward(anchor_image, postive_image)
+                    negative_output = self.model.forward(anchor_image, negative_image)
+                    print(positive_output)
+                    print(negative_output)
 
-                    predicted_index = torch.argmax(output, dim=0)
-                    predicted_index_onehot = F.one_hot(predicted_index)
+                    positive_predicted_index = torch.argmax(positive_output, dim=0)
+                    negative_predicted_index = torch.argmax(negative_output, dim=0)
 
-                    label_merging_tensor = torch.zeros_like(data['same_type'], dtype=torch.long)
-                    print(predicted_index)
-                    print(label_merging_tensor)
-                    print(data['same_type'])
-                    # print(onehot_label1)
-                    # print(onehot_label2)
-                    print('%' * 60)
-                    for i in range(len(data['same_type'])):
-                        if data['same_type'][i]:
-                            label_merging_tensor[i] = label1[i]
-                        else:
-                            label_merging_tensor[i] = label2[i]
+                    print(positive_predicted_index)
+                    print(negative_predicted_index)
 
+                    # predicted_index_onehot = F.one_hot(predicted_index)
+                    #
+                    # label_merging_tensor = torch.zeros_like(data['same_type'], dtype=torch.long)
+                    # print(predicted_index)
+                    # print(label_merging_tensor)
+                    # print(data['same_type'])
+                    # # print(onehot_label1)
+                    # # print(onehot_label2)
+                    # print('%' * 60)
+                    # for i in range(len(data['same_type'])):
+                    #     if data['same_type'][i]:
+                    #         label_merging_tensor[i] = label1[i]
+                    #     else:
+                    #         label_merging_tensor[i] = label2[i]
+                    #
+                    #
+                    # print(label_merging_tensor)
+                    # print(predicted_index_onehot)
+                    # label_merging_tensor = F.one_hot(label_merging_tensor, num_classes=self.num_classes)
+                    # loss = self.loss_fn(y_true=label_merging_tensor, y_pred=predicted_index_onehot)
+                    #
+                    # self.optimizer_fn.zero_grad()
+                    # loss.backward()
+                    # self.optimizer_fn.step()
 
-                    print(label_merging_tensor)
-                    print(predicted_index_onehot)
-                    label_merging_tensor = F.one_hot(label_merging_tensor, num_classes=self.num_classes)
-                    loss = self.loss_fn(y_true=label_merging_tensor, y_pred=predicted_index_onehot)
-
-                    self.optimizer_fn.zero_grad()
-                    loss.backward()
-                    self.optimizer_fn.step()
-
-                    fig, ax = plt.subplots(1, 2)
-                    ax[0].imshow(np.transpose(image1[0].detach().cpu().numpy(), (1, 2, 0)))
-                    ax[1].imshow(np.transpose(image2[0].detach().cpu().numpy(), (1, 2, 0)))
+                    fig, ax = plt.subplots(1, 3)
+                    ax[0].imshow(np.transpose(anchor_image[0].detach().cpu().numpy(), (1, 2, 0)))
+                    ax[1].imshow(np.transpose(postive_image[0].detach().cpu().numpy(), (1, 2, 0)))
+                    ax[2].imshow(np.transpose(negative_image[0].detach().cpu().numpy(), (1, 2, 0)))
                     plt.show()
 
 
