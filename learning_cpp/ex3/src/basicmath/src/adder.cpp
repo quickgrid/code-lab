afb05e895bd85d4fe3dc8f87ac3e10bec4384462
Asif Ahmed <public.quickgrid@gmail.com>
2022-11-12 08:58:01 +0600

    add example 2

--- learning_cpp/ex3/src/basicmath/src/adder.cpp
+++ learning_cpp/ex3/src/basicmath/src/adder.cpp
@@ -6,3 +6,4 @@ namespace bmath{
         return a + b;
     }
 }
+
--- learning_cpp/ex3/src/basicmath/tests/test_basicmath.cpp
+++ learning_cpp/ex3/src/basicmath/tests/test_basicmath.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main(){
     assert(bmath::add(2, 2) == 4);
+    // assert(bmath::add(5.5, 1.2) == 7.7);
+    // assert(bmath::add(2, 2.3) == 4.3);
     assert(bmath::add(4, 0) == 4);
     assert(bmath::add(9, 4) == 13);
     assert(bmath::add(4, 5) == 9);
