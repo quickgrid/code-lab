f36d873bf11d1462e554450982d937633555d332
Asif Ahmed <public.quickgrid@gmail.com>
2022-11-12 06:47:25 +0600

    add cpp files, organize

---
+++ learning_cpp/ex2/src/basicmath/include/mylib/adder.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+
+namespace bmath{
+    int add(int a, int b);
+}
---
+++ learning_cpp/ex2/src/basicmath/tests/test_basicmath.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+
+#include "mylib/adder.hpp"
+
+
+using namespace std;
+
+
+int main(){
+    assert(bmath::add(2, 2) == 4);
+    // assert(bmath::add(5.5, 1.2) == 7.7);
+    // assert(bmath::add(2, 2.3) == 4.3);
+    assert(bmath::add(4, 0) == 4);
+    assert(bmath::add(9, 4) == 13);
+    assert(bmath::add(4, 5) == 9);
+
+    cout << "All tests passed.\n";
+}
